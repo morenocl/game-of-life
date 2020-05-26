033d775599de5d164ca80e092064c33ed86a1bbd
Christian L Moreno <christianluciano.m@gmail.com>
2020-05-25 21:45:31 -0300

    Add parameter time, handler_event function.

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from time import sleep
 liveColor = 250,250,250
 bgColor = 10,10,10
 borderColor = pygame.Color(100,100,100)
-nn, l = 0, 0
+nn, l, t = 0, 0, 0
 screen = None
 
 def configure(args):
-    global nn, l, screen
+    global nn, l, t, screen
     nn = args.n # number of cel
+    t = args.t
     maxSizeCel = args.m # max size of cel, en px
 
     pygame.init()
@@ -43,8 +44,23 @@ def polygon(x, y, l):
             ((x + 1) * l, (y + 1) * l),\
             ((x + 1) * l,    y * l)   ]
 
+def handle_event(pause, newGameStatus):
+    running = True
+    events = pygame.event.get()
+    for ev in events:
+        if ev.type == pygame.QUIT:
+            running = False
+        elif ev.type == pygame.KEYDOWN and ev.key == pygame.K_SPACE:
+            pause = not pause
+
+        mouseClick = pygame.mouse.get_pressed()
+        if sum(mouseClick) > 0:
+            x, y = pygame.mouse.get_pos()
+            i, j = int(np.floor(x / l)), int(np.floor(y / l))
+            newGameStatus[i,j] = not mouseClick[2]
+    return pause, running
+
 def game():
-    global l, screen
     running = True
     pause = True
 
@@ -52,21 +68,7 @@ def game():
 
     while running:
         newGameStatus = np.copy(gameStatus)
-
-        events = pygame.event.get()
-        for ev in events:
-            if ev.type == pygame.QUIT:
-                running = False
-            elif ev.type == pygame.KEYDOWN and ev.key == pygame.K_SPACE:
-                pause = not pause
-
-            mouseClick = pygame.mouse.get_pressed()
-            if sum(mouseClick) > 0:
-                x, y = pygame.mouse.get_pos()
-                i, j = int(np.floor(x / l)), int(np.floor(y / l))
-                newGameStatus[i,j] = not mouseClick[2]
-
-
+        pause, running = handle_event(pause, newGameStatus)
         screen.fill(bgColor)
 
         for y in range(0, nn):
@@ -88,23 +90,25 @@ def game():
 
         gameStatus = np.copy(newGameStatus)
         pygame.display.flip()
-        sleep(.1)
+        sleep(t)
 
-    def au_revoir():
-        # print("\nGracias por probar este juego =D\n")
-        # print("\nThanks for trying this game =D\n")
-        # print("\nMerci d'avoir essayé ce jeu =D\n")
-        # print("\nDankon pro provi ĉi tiun ludon =D\n")
-        # print("\nDeo gratias, quia hoc ludum conatur =D\n")
-        pygame.quit()
+def au_revoir():
+    # print("\nGracias por probar este juego =D\n")
+    # print("\nThanks for trying this game =D\n")
+    # print("\nMerci d'avoir essayé ce jeu =D\n")
+    # print("\nDankon pro provi ĉi tiun ludon =D\n")
+    # print("\nDeo gratias, quia hoc ludum conatur =D\n")
+    pygame.quit()
 
 
 def main():
     parser = argparse.ArgumentParser(description='Game of Life.')
     parser.add_argument('-n', metavar='n', type=int, default=35,
-                        help='number of cels')
+                        help='Number of cels')
     parser.add_argument('-m', metavar='m', type=int, default=20,
-                        help='max size of cel, in px')
+                        help='Max size of cel, in px')
+    parser.add_argument('-t', metavar='t', type=float, default=.1,
+                        help='Time between updates screen')
     args = parser.parse_args()
     configure(args)
     game()
