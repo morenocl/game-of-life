91cf43866b0aa5b410053aa2f1be680468f7c3af
Christian L Moreno <christianluciano.m@gmail.com>
2020-05-26 02:44:16 -0300

    Add some text at right

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import pygame
 from time import sleep
 
 
+textColor = 200,200,200
 liveColor = 250,250,250
 bgColor = 10,10,10
 borderColor = pygame.Color(100,100,100)
-nn, l, t = 0, 0, 0
-screen = None
+nn, l, t, originText = 0, 0, 0, 0
+screen, font = None, None
+TITLE = 'Game of Life'
 
 def configure(args):
-    global nn, l, t, screen
+    global nn, l, t, originText, screen, font, font_title
     nn = args.n # number of cel
     t = args.t
     maxSizeCel = args.m # max size of cel, en px
@@ -21,13 +23,18 @@ def configure(args):
     pygame.init()
     hwWidth  = pygame.display.Info().current_w
     hwHeigth = pygame.display.Info().current_h
+    if not pygame.font.get_init():
+        pygame.font.init()
+    font_title = pygame.font.SysFont('freesans', 26)
+    font = pygame.font.SysFont('freesans', 18)
     # sets the width of a cell depending on the amount to be displayed.
     l = int(hwHeigth / nn) if int(hwHeigth / nn) < maxSizeCel else maxSizeCel
 
     # calculates the width of the screen based on the width of each cell.
     screenHeigth = (l * nn) if (l * nn) < hwHeigth else hwHeigth
+    originText = screenHeigth + 10
     # for now it shows only one square of cells. Then it will show information on the right.
-    sizeScreen = (screenHeigth, screenHeigth)
+    sizeScreen = (screenHeigth + 200, screenHeigth)
     screen = pygame.display.set_mode(sizeScreen)
 
 def getNumberNeighbor(gameStatus, coor):
@@ -61,15 +68,28 @@ def handle_event(pause, newGameStatus):
     return pause, running
 
 def game():
+    i = 0
     running = True
     pause = True
 
+    # Initial status of game
     gameStatus = np.zeros((nn, nn))
 
     while running:
         newGameStatus = np.copy(gameStatus)
         pause, running = handle_event(pause, newGameStatus)
         screen.fill(bgColor)
+        # Set the initial text of game
+        title = font_title.render(TITLE, True, textColor)
+
+        info1 = font.render('Cells: {0} x {0}'.format(nn), True, textColor)
+        info2 = font.render('Iteration: {0}'.format(i), True, textColor)
+        originY = 0
+        screen.blit(title, (originText, originY))
+        originY = font_title.size(TITLE)[1] + 10
+        screen.blit(info1, (originText, originY))
+        originY = originY + font.size('Cells: {0} x {0}'.format(nn))[1] + 5
+        screen.blit(info2, (originText, originY))
 
         for y in range(0, nn):
             for x in range(0, nn):
@@ -88,6 +108,7 @@ def game():
                 else:
                     pygame.draw.polygon(screen, borderColor, polygon(x, y, l), 1)
 
+        i = i + 1 if (not pause) and not (gameStatus == newGameStatus).all() else i
         gameStatus = np.copy(newGameStatus)
         pygame.display.flip()
         sleep(t)
@@ -98,6 +119,7 @@ def au_revoir():
     # print("\nMerci d'avoir essayé ce jeu =D\n")
     # print("\nDankon pro provi ĉi tiun ludon =D\n")
     # print("\nDeo gratias, quia hoc ludum conatur =D\n")
+    pygame.font.quit()
     pygame.quit()
 
 
